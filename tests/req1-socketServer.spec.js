866f641492f4ebc8525f4d8dd76206e47a837ebe
Eder Santos <3derms@gmail.com>
2022-01-23 00:28:20 -0300

    Corrige variavel para requisito

--- tests/req1-socketServer.spec.js
+++ tests/req1-socketServer.spec.js
@@ -3,7 +3,7 @@ const io = require('socket.io-client');
 const BASE_URL = 'http://localhost:3000/';
 
 describe('1 - Crie um back-end para conexão simultânea de clientes e troca de mensagens em chat público', () => {
-  const message = 'We can only see a short distance ahead, but we can see plenty there that needs to be done.';
+  const chatMessage = 'We can only see a short distance ahead, but we can see plenty there that needs to be done.';
   const nickname = 'Alan Turing';
 
   let client1;
@@ -22,20 +22,20 @@ describe('1 - Crie um back-end para conexão simultânea de clientes e troca de 
     client2 = io.connect(BASE_URL, { reconnection: false });
     client3 = io.connect(BASE_URL, { reconnection: false });
 
-    client1.emit('message', { message, nickname });
+    client1.emit('message', { chatMessage, nickname });
 
     client1.on('message', (message) => {
-      expect(message.includes(message)).toBeTruthy();
+      expect(message.includes(chatMessage)).toBeTruthy();
       expect.assertions(1);
     });
 
     client2.on('message', (message) => {
-      expect(message.includes(message)).toBeTruthy();
+      expect(message.includes(chatMessage)).toBeTruthy();
       expect.assertions(2);
     });
 
     client3.on('message', (message) => {
-      expect(message.includes(message)).toBeTruthy();
+      expect(message.includes(chatMessage)).toBeTruthy();
       expect.assertions(3);
       done();
     });
@@ -48,16 +48,16 @@ describe('1 - Crie um back-end para conexão simultânea de clientes e troca de 
     client1 = io.connect(BASE_URL, { reconnection: false });
     client2 = io.connect(BASE_URL, { reconnection: false });
 
-    client1.emit('message', { message, nickname });
+    client1.emit('message', { chatMessage, nickname });
 
     client1.on('message', (message) => {
-      expect(message.includes(message)).toBeTruthy();
+      expect(message.includes(chatMessage)).toBeTruthy();
       expect(message.includes(nickname)).toBeTruthy();
       expect(message).toMatch(dateRegex);
       expect(message).toMatch(timeRegex);
     });
     client2.on('message', (message) => {
-      expect(message.includes(message)).toBeTruthy();
+      expect(message.includes(chatMessage)).toBeTruthy();
       expect(message.includes(nickname)).toBeTruthy();
       expect(message).toMatch(dateRegex);
       expect(message).toMatch(timeRegex);
