e95c6d9f1e8e24c512b0d5a22e4d2dca39993657
Eder Santos <3derms@gmail.com>
2022-01-22 22:52:20 -0300

    Cria um frontend para que as pessoas interajam com o chat.

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,6 +5,42 @@ const socket = window.io();
  * Link do repositório: https://github.com/tryber/sd-011-project-webchat/pull/7/files
  */
 
+ const msgForm = document.querySelector('#msg-form');
+ const msgInput = document.querySelector('#msg-input');
+ const nickForm = document.querySelector('#nick-form');
+ const nickInput = document.querySelector('#nickname');
+ 
+ const DATA_TESTID = 'data-testid';
+
+ const randomNickGenerator = () => {
+  let nickname = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 16; i += 1) {
+    nickname += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  sessionStorage.setItem('nickname', nickname);
+  socket.emit('userConnected ', nickname);
+  return nickname;
+};
+
+socket.on('userConnected ', (usersStorage) => {
+  const userOnline = document.querySelector('#user-online');
+  userOnline.innerHTML = '';
+  const currentNickName = sessionStorage.getItem('nickname');
+  usersStorage.forEach(({ nickname }) => {
+    const nickLi = document.createElement('li');
+    nickLi.innerText = currentNickName;
+    nickLi.setAttribute(DATA_TESTID, 'online-user');
+    if (nickname !== currentNickName) {
+      nickLi.innerText = nickname;
+      nickLi.setAttribute(DATA_TESTID, 'online-user');
+      userOnline.appendChild(nickLi);
+    } else {
+      userOnline.insertBefore(nickLi, userOnline.firstChild);
+    }
+  });
+});
+
 const createMessage = (message) => {
   const messages = document.querySelector('#messages');
   const li = document.createElement('li');
@@ -14,4 +50,29 @@ const createMessage = (message) => {
   window.scrollTo(0, document.body.scrollHeight);
 };
 
+nickForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const nickname = nickInput.value;
+  if (nickInput.value) {
+    socket.emit('updateNickname', nickname);
+    sessionStorage.setItem('nickname', nickname);
+  }
+  return false;
+});
+
+msgForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const getNick = sessionStorage.getItem('nickname');
+  const message = msgInput.value;
+  if (msgInput.value) {
+    socket.emit('message', { message, nickname: getNick });
+    msgInput.value = '';
+  }
+  return false;
+});
+
 socket.on('message', (message) => createMessage(message));
+
+window.onload = () => {
+  randomNickGenerator();
+};
--- sockets/user.js
+++ sockets/user.js
@@ -16,6 +16,24 @@ const connectUser = (socket, io) => {
   });
 };
 
+const updateNickname = (socket, io) => {
+  socket.on('updateNickname', (newNickName) => {
+    const findUser = usersStorage.findIndex((user) => user.id === socket.id);
+    usersStorage[findUser].nickname = newNickName;
+    io.emit('userConnected ', usersStorage);
+  });
+};
+
+const disconnectUser = (socket) => {
+  socket.on('disconnect', () => {
+    const disconnectedUser = usersStorage.find((user) => user.id === socket.id);
+    usersStorage.splice(usersStorage.indexOf(disconnectedUser), 1);
+    socket.broadcast.emit('userConnected ', usersStorage);
+  });
+};
+
 module.exports = {
   connectUser,
+  updateNickname,
+  disconnectUser,
 };
