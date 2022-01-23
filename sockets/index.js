9f8fb4af3ceaa8172324509ea1bdc259aa83e8da
Eder Santos <3derms@gmail.com>
2022-01-22 23:08:52 -0300

    Corrige index.js para informar a todos os clientes quem está online no momento

--- sockets/index.js
+++ sockets/index.js
@@ -2,11 +2,13 @@ const message = require('./message');
 const user = require('./user');
 
 const { messageBody } = message;
-const { connectUser } = user;
+const { connectUser, updateNickname, disconnectUser } = user;
 
 module.exports = (io) => {
   io.on('connection', (socket) => {
     connectUser(socket, io);
     messageBody(socket, io);
+    updateNickname(socket, io);
+    disconnectUser(socket);
   });
 };
